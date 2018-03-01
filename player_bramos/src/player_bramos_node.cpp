3bed4e594d66487411ec65ccaebc4cbdb32834e6
sapofree <B.Ramos@ua.pt>
2018-03-01 10:45:03 +0000

    create player name

--- player_bramos/src/player_bramos_node.cpp
+++ player_bramos/src/player_bramos_node.cpp
@@ -1,7 +1,24 @@
 #include <iostream>
 
+class Player
+{
+public:
+  Player(std::string argin_name)
+  {
+    // this->name = name;
+    name = argin_name;
+  }
+
+  std::string name;
+
+private:
+};
+
 int main()
 {
-  std::cout << "Hello world" << std::endl;
-  return 1;
+  std::string player_name = "bramos";
+  // Creating an instance of class Player
+  Player player(player_name);
+
+  std::cout << "Created an instance of class player with public name " << player.name << std::endl;
 }
