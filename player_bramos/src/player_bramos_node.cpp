f03a9ed080e28754071f912bc49f1813539e7852
sapofree <B.Ramos@ua.pt>
2018-03-02 10:18:30 +0000

    sry guys

--- player_bramos/src/player_bramos_node.cpp
+++ player_bramos/src/player_bramos_node.cpp
@@ -122,6 +122,7 @@ public:
 
   void printReport()
   {
+    ROS_INFO_STREAM("My name is " << name << " and my team is " << getTeamName().c_str() << endl);
     ROS_INFO("My name is %s and my team is %s", name.c_str(), getTeamName().c_str());
   }
 };
@@ -139,7 +140,6 @@ int main(int argc, char** argv)
   ros::Rate loop_rate(10);
   while (ros::ok())
   {
-    ROS_INFO_STREAM("My name is " << name << " and my team is " << getTeamName().c_str() << endl);
     my_player.move();
 
     ros::spinOnce();
